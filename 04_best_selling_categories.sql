e4623531771fc558c791fa00f111beb0a39ce623
lutfiusluer <byusluerdev@gmail.com>
2025-04-25 12:49:05 +0300

    add daily practice

--- 04_best_selling_categories.sql
+++ 04_best_selling_categories.sql
@@ -1,7 +1,20 @@
 --  Task 4: List categories by total revenue.
 --For each product category, calculate:
-                -- the total revenue from all its products (based on order details)
+-- the total revenue from all its products (based on order details)
 
-select * from products;
-select * from order_details;
-select * from categories
+with po_revenue as
+         (select product_id,
+                 (unit_price * quantity) as total_revenue
+          from order_details
+          group by product_id, order_id)
+
+select c.category_id, category_name, round(sum(po_revenue.total_revenue)::numeric, 2) as total_revenue
+from categories c
+         join products p on c.category_id = p.category_id
+         join po_revenue on po_revenue.product_id = p.product_id
+group by c.category_id, category_name
+order by total_revenue DESC;
+
+
+
+
